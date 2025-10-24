7d7911b2d4adf02d83ddc8116aa952e8b382aec1
AlexandruCj <pbazcoj2912@g.educaand.es>
2025-10-24 14:14:45 +0200

    Update MediaNotas.java

--- src/tema2_debug/MediaNotas.java
+++ src/tema2_debug/MediaNotas.java
@@ -7,8 +7,7 @@ public class MediaNotas {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		int contador = 1; // le ponemos que vale 1 para que el mensaje no salga "introduce la nota del
-							// alumno 0, sino que empiece desde alumno 1
+		int contador = 1; // le ponemos que vale 1 para que el mensaje no salga "introduce la nota del alumno 0, sino que empiece desde alumno 1
 		double suma = 0;
 
 		System.out.print("Introduce el número de alumnos: ");
@@ -20,17 +19,17 @@ public class MediaNotas {
 
 			if (nota < 0 || nota > 10) {
 				System.out.println("Error: la nota debe estar entre 0 y 10. Intenta de nuevo.");
-				// No se suma ni se avanza el contador
-				continue; // Vuelve a pedir la nota
+			} else {
+				suma = suma + nota;
+				contador++; // ponemos un else para que asi no tenga en cuenta el numero in valido y asi no lo sume a la media y te pida otro vez la nota de ese alumno
 			}
-
-			suma = suma + nota;
-			contador++;
+			}
 		}
 
 		double media = suma / numAlumnos;
-		System.out.println("La media del grupo es: " + media);
+		System.out.println("La media del grupo es: %.2f" + media); //le ponemos "%.ef" para que asi redonde a dos numeros decimales y no a infinitos.
 
 		sc.close();
 	}
 }
+
